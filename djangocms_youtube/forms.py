4d2fb7d8a511d5bdb8a549bf95efe8f0b9acb576
Mishbah Razzaque <mishbah@jp74.com>
2015-08-26 23:32:24 +0100

    Validate `video_url` in YoutubeModelForm.

--- djangocms_youtube/forms.py
+++ djangocms_youtube/forms.py
@@ -1,3 +1,7 @@
+from __future__ import unicode_literals
+
+import re
+
 from django import forms
 
 from .models import Youtube
@@ -12,3 +16,16 @@ class YoutubeModelForm(forms.ModelForm):
         widgets = {
             'video_url': YoutubeVideoURLWidget(),
         }
+
+    def clean_video_url(self):
+        video_url = self.cleaned_data['video_url']
+        pattern = (
+            r'(https?://)?(www\.)?'
+            '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+            '(watch\?v=|embed/|v/|.+\?v=)?([\w-]{11})')
+
+        match = re.match(pattern, video_url)
+        if not match:
+            raise forms.ValidationError('The YouTube video URL is invalid')
+
+        return video_url

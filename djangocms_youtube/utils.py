c08773f780385bdb5a4a6cc83d610d0e53be27dc
Mishbah Razzaque <mishbah@jp74.com>
2015-08-27 12:50:34 +0100

    Refactor utils —> get_video_details()
    — Added logic to fetch categoryName.

--- djangocms_youtube/utils.py
+++ djangocms_youtube/utils.py
@@ -3,34 +3,62 @@ from __future__ import unicode_literals
 
 from django.utils.encoding import force_text
 from django.utils.translation import ugettext_lazy as _
+
 from requests.api import request
 from requests.exceptions import RequestException
 
 from .conf import settings
-from .exceptions import YoutubeAPIError
+from .exceptions import YoutubeAPI404, YoutubeAPIError
 
 
-def get_video_details(video_id):
+def fetch_gdata(resource, parts, identifier):
     access_token = settings.DJANGOCMS_YOUTUBE_API_KEY
+
     if access_token is None:
         msg = _('Missing DJANGOCMS_YOUTUBE_API_KEY settings')
         raise YoutubeAPIError(500, 'ImproperlyConfigured', force_text(msg))
 
-    yt_endpoint = \
-        'https://www.googleapis.com/youtube/v3/videos' \
-        '?part=snippet,player,status,contentDetails&id={0}&key={1}'.format(
-            video_id, access_token)
+    url_template = '{resource}?part={parts}&fields=items&maxResults=1&id={id}&key={access_token}'
+    url = url_template.format(
+        resource=resource,
+        parts=','.join(parts),
+        id=identifier,
+        access_token=access_token,
+    )
 
+    base_url = 'https://www.googleapis.com/youtube/v3/'
     try:
-        response = request('get', url=yt_endpoint)
+        response = request('get', url='{base_url}{url}'.format(base_url=base_url, url=url))
         response.raise_for_status()
     except RequestException as e:
         error = e.response.json().get('error')
         raise YoutubeAPIError(error['code'], 'YoutubeAPIError', error['message'])
     else:
-        results = response.json().get('items', [])
-        if not results:
-            msg = _('This video does not exist.')
-            raise YoutubeAPIError(404, 'videoNotFound', force_text(msg))
-        else:
-            return results[0]
+        items = response.json().get('items', [])
+        if not items:
+            raise YoutubeAPI404()
+        return items[0]
+
+
+def get_video_details(video_id):
+    parts = ('snippet', 'player', 'status', 'contentDetails', )
+    try:
+        result = fetch_gdata('videos', parts, video_id)
+    except YoutubeAPI404:
+        msg = _('This video does not exist.')
+        raise YoutubeAPIError(404, 'videoNotFound', force_text(msg))
+    else:
+        category_name = get_category_name(result.get('snippet', {}).get('categoryId', ''))
+        result['snippet']['categoryName'] = category_name
+        return result
+
+
+def get_category_name(cat_id):
+    parts = ('snippet', )
+    try:
+        result = fetch_gdata('videoCategories', parts, cat_id)
+    except YoutubeAPI404:
+        msg = _('This video category does not exist.')
+        raise YoutubeAPIError(404, 'videoCategoryNotFound', force_text(msg))
+    else:
+        return result.get('snippet', {}).get('title', '')

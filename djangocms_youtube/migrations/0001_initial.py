c2ebd6da0bb39e2a39f2540badff9b8b40c45bc1
Nick Avgerinos <nicka@axcella.com>
2020-12-24 11:31:50 -0800

    migration updates for django

--- djangocms_youtube/migrations/0001_initial.py
+++ djangocms_youtube/migrations/0001_initial.py
@@ -21,7 +21,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Youtube',
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True,
+                    auto_created=True, primary_key=True, serialize=False,
+                    to='cms.CMSPlugin',
+                    on_delete=django.db.models.deletion.CASCADE)),
                 ('title', models.CharField(max_length=150, verbose_name='Title', blank=True)),
                 ('video_url', models.URLField(help_text='Paste the URL of the YouTube video', verbose_name='Video URL')),
                 ('width', models.PositiveIntegerField(help_text='Sets the width of your player, used on some templates where applicable', null=True, verbose_name='Width', blank=True)),
